19a2be21d868668eaaba501f4503e59b4cfa884d
Zakordonets Sergey <posar4@mail.ru>
2020-02-28 02:06:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,32 +6,31 @@ const cache_name = `chess-${version}`;
  */
 self.addEventListener('install', (event) => {
     console.dir('[SV install]', event, event.target);
-
-    event.waitUntil(caches
-        .open(cache_name)
-        .then((cache) => {
-            console.log(cache);
-            cache
-                .addAll([
-                    `/chess/`,
-                    `/chess/index.html`,
-                    `/chess/preload.html`,
-                    `/chess/manifest.json`,
-                    `/chess/icons-512.png`,
-                    `/chess/style.css`,
-                    `/chess/chess.js`,
-                ])
-                .then(function () {
-                    console.log(arguments);
-                    self.skipWaiting()
-                })
-                .catch((exception) => {
-                    console.dir(exception);
-                });
-        })
-        .catch((exception) => {
-            console.dir(exception);
-        }));
+    // event.waitUntil(caches
+    //     .open(cache_name)
+    //     .then((cache) => {
+    //         console.log(cache);
+    //         cache
+    //             .addAll([
+    //                 `/chess/`,
+    //                 `/chess/index.html`,
+    //                 `/chess/preload.html`,
+    //                 `/chess/manifest.json`,
+    //                 `/chess/icons-512.png`,
+    //                 `/chess/style.css`,
+    //                 `/chess/chess.js`,
+    //             ])
+    //             .then(function () {
+    //                 console.log(arguments);
+    //                 self.skipWaiting()
+    //             })
+    //             .catch((exception) => {
+    //                 console.dir(exception);
+    //             });
+    //     })
+    //     .catch((exception) => {
+    //         console.dir(exception);
+    //     }));
 });
 
 /**
@@ -39,7 +38,7 @@ self.addEventListener('install', (event) => {
  */
 self.addEventListener('activate', (event) => {
     console.dir('[SV activate]', event, event.target);
-    event.waitUntil(self.clients.claim());
+    // event.waitUntil(self.clients.claim());
 });
 
 /**
@@ -47,7 +46,7 @@ self.addEventListener('activate', (event) => {
  */
 self.addEventListener('fetch', (event) => {
     console.dir('[SV fetch]', event, event.target);
-    event.respondWith(caches.open(cache_name)
-        .then(cache => cache.match(event.request, { ignoreSearch: true }))
-        .then(response => response || fetch(event.request)));
+    // event.respondWith(caches.open(cache_name)
+    //     .then(cache => cache.match(event.request, { ignoreSearch: true }))
+    //     .then(response => response || fetch(event.request)));
 });
